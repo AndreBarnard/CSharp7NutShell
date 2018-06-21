d1da7ec26383a368936feec0f28b9bec1ac85610
abarnard <abarnard@singular.co.za>
2018-06-21 22:07:15 +0200

    Initial upload

---
+++ C7NutShell/Point.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp7NutShell
+{
+	public struct StructPoint { public int X, Y; }
+
+	public class ClassPoint { public int X, Y; }
+}
